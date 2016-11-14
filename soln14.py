67b78ea41097ef84a5b3faadc600ba7ca446ad7d
Manick Mehra <manickmehra2006@yahoo.com>
2016-11-15 01:22:05 +0530

    Added the CapGain Q14

---
+++ soln14.py
@@ -0,0 +1,95 @@
+class ArrayQueue:
+	""" FIFO queue implementation using a Python list as underlying storage """
+	DEFAULTY_CAPACITY = 2
+
+	def __init__ (self):
+		""" create an empty queue """
+		self._data = [None] * ArrayQueue.DEFAULTY_CAPACITY
+		self._size = 0
+		self._front = 0
+
+	def len(self):
+		""" Return the number of elements in the queue """
+		return self._size
+
+	def capacity(self):
+		return len(self._data)
+
+	def is_empty(self):
+		""" Return True if the queue is empty """
+		return self._size == 0
+
+	def first(self):
+		""" Return (not remove) the element at the fron of the queue
+		"""
+		if self.is_empty():
+			print('Queue is empty')
+		else:
+			return self._data[self._front]
+
+	def dequeue(self):
+		""" Remove and return the front element of the queue		
+		"""
+
+		if self.is_empty():
+			print('Queue is empty')
+		else:			
+			answer = self._data[self._front]
+			self._data[self._front] = None
+			self._front = (self._front + 1) % len(self._data)
+			self._size -= 1
+			if 0 < self._size < len(self._data) // 4:
+				self._resize(len(self._data) // 2)
+			return answer
+
+	def enqueue(self, e):
+		""" Add an element to the back of queue """
+
+		if self._size == len(self._data):				# if queue elements == queue capacity, allow queue capacity to grow 2* present capacity
+			self._resize(2 * len(self._data))
+		avail = (self._front + self._size) % len(self._data)
+		self._data[avail] = e
+		self._size += 1
+
+	def _resize( self, cap):
+		""" Resize to a new list of capacity >= len(self) """
+
+		old = self._data
+		self._data = [None] * cap
+		walk = self._front
+		for k in range(self._size):
+			self._data[k] = old[walk]
+			walk = (1 + walk) % len(old)
+		self._front = 0
+
+class capGain:
+
+	totCapGain = 0
+	shareHeld = ArrayQueue()
+
+	def menuPrompt(self):
+		filename = input("WELCOMEE!!!!!:\t")
+    	menu()
+    	self._choice= int(input("Enter menu choice:\t"))
+    	while self._choice != 5:
+    		if self._choice == 1:
+    			buyShare(numberOfShares,price)
+        	elif self._choice == 2:
+           	 #Sell a share
+            	read(filename)
+        	elif self._choice == 3:
+          	  #Calculate total capita gain
+         		append(filename)
+       		 elif self._choice == 4:
+           		 #get total
+           		get_total(filename)
+
+    def menu(self):
+		#user chooses a number from list
+    	print("Choose a number to continue:\t\n\
+	    1 : Buy a Share\n\
+	    2 : Sell the share\n\
+	    3 : Total Capital Gain So far\n\
+	    4 : Quit")
+
+    	print("\nApplication Complete")
